d3c2f68f7c5865af32bde43c7ab63f6dcde3dda6
cruznavj <joan.cruz@roche.com>
2023-03-25 08:19:40 +0100

    upload files

--- README.md
+++
@@ -1,1 +0,0 @@
-"# MasterLemoncode Api Rest" 
--- src/pods/character-collection/character-collection.vm.ts
+++
@@ -1,8 +0,0 @@
-export interface CharacterEntityVm {
-  id: string;
-  picture: string;
-  name: string;
-  description: string;
-  specie: string;
-  location: string;
-}
